a360363d55234212279378c41de180f167d94cc7
Aqudi <gjdigj145@gmail.com>
2021-03-19 02:48:00 +0900

    feat: Add utils and constants

---
+++ lib/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import 'utils/hex_color.dart';
+
+Color likelionOrange = HexColor("#FF9E1B").withOpacity(0.82);
+Color blueGray300 = Colors.blueGrey.shade300;
