d15e103d488f22cda26e32b5b859f4c653567c7e
Aqudi <gjdigj145@gmail.com>
2021-03-19 14:26:27 +0900

    feat: Apply theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:msof_front/color.dart';
 import 'package:msof_front/pages/home/home_page.dart';
 
 void main() {
@@ -12,8 +13,25 @@ class MSOF extends StatelessWidget {
     return MaterialApp(
       title: 'MutstackOverflow',
       theme: ThemeData(
-        fontFamily: 'NotoSans',
         primarySwatch: Colors.grey,
+        backgroundColor: Colors.white,
+        cardColor: likelionOrange,
+        primaryTextTheme: TextTheme(
+          button: TextStyle(
+            color: Colors.black87,
+            decorationColor: Colors.black38,
+          ),
+          subtitle2: TextStyle(
+            color: Colors.blueGrey[900],
+          ),
+          subtitle1: TextStyle(
+            color: Colors.black87,
+          ),
+          headline1: TextStyle(color: Colors.blueGrey[800]),
+        ),
+        bottomAppBarColor: Colors.black87,
+        iconTheme: IconThemeData(color: Colors.white),
+        fontFamily: 'NotoSans',
       ),
       home: HomePage(),
     );
