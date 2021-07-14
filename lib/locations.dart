b3a1d0c4c077c8d8a51184b9f04c5d39d4542466
Aqudi <gjdigj145@gmail.com>
2021-07-14 16:10:35 +0900

    feat: Apply route title and logger

--- lib/locations.dart
+++ lib/locations.dart
@@ -3,7 +3,7 @@ import 'package:flutter/src/widgets/framework.dart';
 
 import 'package:msof_front/pages/pages.dart';
 import 'package:msof_front/routes.dart';
-import 'package:msof_front/utils/string_extensions.dart';
+import 'package:msof_front/utils/logger.dart';
 
 class BeamerLocations extends BeamLocation {
   BeamerLocations({BeamState? state}) : super(state);
@@ -13,35 +13,36 @@ class BeamerLocations extends BeamLocation {
 
   @override
   List<BeamPage> buildPages(BuildContext context, BeamState state) {
+    logger.d('${state.uri.pathSegments}, ${state.pathParameters}');
     return [
       if (state.uri.pathSegments.contains(Routes.notFound))
         BeamPage(
           key: ValueKey(Routes.notFound),
-          title: Routes.notFound.capitalizeFirsLetter,
+          title: Routes.getRouteInfoByRouteName(Routes.notFound).title,
           child: NotFoundPage(),
         ),
       if (state.uri.pathSegments.contains(Routes.login))
         BeamPage(
           key: ValueKey(Routes.login),
-          title: Routes.login.capitalizeFirsLetter,
+          title: Routes.getRouteInfoByRouteName(Routes.login).title,
           child: LoginPage(),
         ),
       if (state.uri.pathSegments.contains(Routes.logout))
         BeamPage(
           key: ValueKey(Routes.logout),
-          title: Routes.logout.capitalizeFirsLetter,
+          title: Routes.getRouteInfoByRouteName(Routes.logout).title,
           child: LogoutPage(),
         ),
       if (state.uri.pathSegments.contains(Routes.signup))
         BeamPage(
           key: ValueKey(Routes.signup),
-          title: Routes.signup.capitalizeFirsLetter,
+          title: Routes.getRouteInfoByRouteName(Routes.signup).title,
           child: SignupPage(),
         ),
       if (state.uri.pathSegments.contains(Routes.home))
         BeamPage(
           key: ValueKey(Routes.home),
-          title: Routes.home.capitalizeFirsLetter,
+          title: Routes.getRouteInfoByRouteName(Routes.home).title,
           child: HomePage(),
         ),
     ];
